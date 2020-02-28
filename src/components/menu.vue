66d9919fb338352be09746f336f57b47d5a5d42a
shivanishetty20 <shivanisunder1999@gmail.com>
2020-02-28 08:58:45 +0530

    update table

--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,32 +1,34 @@
 <template> 
 <div class="left">
-        <table>
+  <br>
+  {{buses[0]}}
+       <table>
         <thead>
           <tr>
               <th>Bus Name</th>
-              <td>{{fire.ZH9lzDYt3iOKwXrFg5PsEd7bH043.buses.KA19MJ7646.bus_name}}</td>
+              <td>{{fire[0].buses.KA19MJ7646.bus_name}}</td>
           </tr>
         </thead>
         <tbody>
           <tr>
               <th>Bus Number</th>
-              <td>{{fire.ZH9lzDYt3iOKwXrFg5PsEd7bH043.buses.KA19MJ7646.bus_number}}</td>
+              <td>{{fire[0].buses.KA19MJ7646.bus_number}}</td>
           </tr>
           <tr>
               <th>Driver Name</th>
-              <td>{{fire.ZH9lzDYt3iOKwXrFg5PsEd7bH043.buses.KA19MJ7646.driver_details.name}}</td>
+              <td>{{fire[0].buses.KA19MJ7646.driver_details.name}}</td>
           </tr>
           <tr>
               <th>Driver Number</th>
-              <td>{{fire.ZH9lzDYt3iOKwXrFg5PsEd7bH043.buses.KA19MJ7646.driver_details.number}}</td>
+              <td>{{fire[0].buses.KA19MJ7646.driver_details.number}}</td>
           </tr>
           <tr>
               <th>Conductor Name</th>
-              <td>{{fire.ZH9lzDYt3iOKwXrFg5PsEd7bH043.buses.KA19MJ7646.conductor_details.name}}</td>
+              <td>{{fire[0].buses.KA19MJ7646.conductor_deatils.name}}</td>
           </tr>
           <tr>
               <th>Conductor Number</th>
-              <td>{{fire.ZH9lzDYt3iOKwXrFg5PsEd7bH043.buses.KA19MJ7646.conductor_details.number}}</td>
+              <td>{{fire[0].buses.KA19MJ7646.conductor_deatils.number}}</td>
           </tr>
         </tbody>
       </table>         
@@ -35,14 +37,39 @@
 
 <script>
 import firebase from '../firebaseconfig.js'
-
-var ex;
-firebase.database().ref('/clients').on('value',snap=>{ex=snap.val()})
+// import firebase from 'firebase'
 export default {
 data() {
-    return {
-        fire: ex
+   return {
+        fire: [],
+        buses:[],
+        uid:"",
+        bus_number:[]
+
     }
-},    
+},created(){
+firebase.auth().onAuthStateChanged(user=>{
+  if(user){
+  this.uid=user.uid
+  // console.log(this.uid)
+  this.fetchdeatils()
+  }
+})
+
+
+
+}  ,
+methods:{
+  fetchdeatils(){
+firebase.database().ref('/clients/'+this.uid).on('value',snap=>{
+  
+  this.fire.push(snap.val())
+  this.buses.push(snap.val().buses)
+  this.bus_number.push(Object.keys(this.buses[0]))
+  console.log(Object.keys(this.fire[0]))
+
+  })
+  }
+}  
 }
 </script>
